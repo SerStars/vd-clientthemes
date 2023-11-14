6f3ebcb411c44a76dcd75aeff4d91d28177397f6
SerStars <90714930+SerStars@users.noreply.github.com>
2023-11-14 23:53:04 +0100

    Create index.ts

---
+++ ClientThemesFix/src/index.ts
@@ -0,0 +1,20 @@
+import { find, findByProps } from "@vendetta/metro"
+import { instead, after } from "@vendetta/patcher"
+import { storage } from "@vendetta/plugin"
+
+const AppearanceSettings = findByProps("setShouldSyncAppearanceSettings")
+const canUse = findByProps('canUseClientThemes');
+
+storage.isEnabled ??= false
+
+
+AppearanceSettings.setShouldSyncAppearanceSettings(false)
+
+const patches = [
+    instead("setShouldSyncAppearanceSettings", AppearanceSettings, () => !storage.isEnabled),
+    instead(canUse, "canUseClientThemes", () => () => true)
+]
+
+export const onUnload = () => {
+    for (const unpatch of patches) unpatch()
+}
